819ee3136f4a7b7366ceac75c1cd166298068001
caojia <jerrycircle@gmail.com>
2012-09-04 10:07:59 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CunxinUi::Application.config.secret_token = 'ac0de94775ade3403febaa65fa85033103d4f9373ffb6d082ad782a83dea764a6d890219850ad0172671703dbe79cdff2b5f2831d748acc70358522a6943a26e'
