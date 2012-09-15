ea8aa541ca7a58d27b459b072e78400ecd55fd75
caojia <jerrycircle@gmail.com>
2012-09-15 21:06:51 +0800

    update readme and prepare seeds data

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 This is UI/Front End repo for Cunxin Project
+
+== Setup on CentOS
+# yum install mysql55-server gcc make zlib-devel openssl-devel libxml2-devel libxslt-devel mysql55-devel sqlite-devel -y 
+# yum localinstall --nogpgcheck http://nodejs.tchol.org/repocfg/amzn1/nodejs-stable-release.noarch.rpm && yum install nodejs
+# install rvm (curl -L https://get.rvm.io | bash -s stable --ruby) && echo 'source /home/deploy/.rvm/scripts/rvm' >> ~/.bashrc)
+# git clone https://github.com/willamette/cunxin-ui.git
+# rvm install ruby-1.9.3-p0
+# gem install bundler
+# bundle install
+# rake db:migrate 
+# ruby config/seed.rb
+# rails s -p 3000 -e development -d
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,172 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# encoding: UTF-8
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user2.name
-user.add_role :admin
+
+project = Project.create(
+  :canonical_name => "project1", 
+  :headline => "爱心召集：给苏州12岁白血病女孩微微再一次生命！",
+  :description => "她叫小薇薇,12岁,去年被查出白血病.为了给她看病,薇薇妈说能借的都借了,6岁弟弟为她用拼音写求助信乞讨,奶奶为她瞒着家人捡垃圾筹医药费...她说她想上学,想去苏州乐园,还想和同学一起去秋游...于是微博网友用爱心PS带她去了好多地方,可她的病还需要花很多很多钱,你愿意再给她一次生命希望吗？",
+  :location => "苏州",
+  :thumbnail_small => "/projects/1.jpg",
+  :thumbnail_large => "/projects/1.jpg",
+  :start_date => "2012-09-08 00:00:00",
+  :donators_count => 1000,
+  :target_amount => 50000
+)
+
+Carousel.create(
+  :project_id => project.id,
+  :position => 1
+)
+
+project = Project.create(
+  :canonical_name => "project2", 
+  :headline => "【爱心衣橱】紧急为云南地震灾区小学生筹集新衣项目款",
+  :description => "2012年9月7日11时19分，云南省昭通市彝良县发生里氏5.7级地震。截至9月9日11时，地震已造成81人死亡，821人受伤，20.1万人紧急转移安置。",
+  :location => "云南",
+  :thumbnail_small => "/projects/2.jpg",
+  :thumbnail_large => "/projects/2.jpg",
+  :start_date => "2012-09-01 00:00:00",
+  :donators_count => 1000,
+  :target_amount => 50000
+)
+
+Carousel.create(
+  :project_id => project.id,
+  :position => 2
+)
+
+charity = Charity.create(
+  canonical_name: "charity1",
+  name: "壹基金",
+  thumbnail_large: "/charities/1.jpeg",
+  thumbnail_small: "/charities/1.jpeg",
+  published: true,
+  description: "chartity 1",
+  short_description: "charity 2",
+  published_at: Time.now.utc)
+
+
+charity = Charity.create(
+  canonical_name: "charity2",
+  name: "免费午餐",
+  thumbnail_large: "/charities/2.jpeg",
+  thumbnail_small: "/charities/2.jpeg",
+  published: true,
+  description: "chartity 2",
+  short_description: "charity 2",
+  total_amount: 1000000,
+  published_at: Time.now.utc
+)
+
+
+charity = Charity.create(
+  canonical_name: "charity3",
+  name: "chartity 3",
+  thumbnail_large: "/charities/3.jpeg",
+  thumbnail_small: "/charities/3.jpeg",
+  published: true,
+  description: "chartity 3",
+  short_description: "charity 3",
+  published_at: Time.now.utc
+)
+
+charity = Charity.create(
+  canonical_name: "charity4",
+  name: "chartity 4",
+  thumbnail_large: "/charities/4.jpeg",
+  thumbnail_small: "/charities/4.jpeg",
+  published: true,
+  total_amount: 100000,
+  description: "chartity 4",
+  short_description: "charity 4",
+  published_at: Time.now.utc
+)
+
+consult = Consult.create(
+  canonical_name: "consult1",
+  name: "王庆",
+  thumbnail_large: "/consults/1.jpeg",
+  thumbnail_small: "/consults/1.jpeg",
+  published: true,
+  description: "bla bla bla...",
+  short_description: "bla bla bla...",
+  published_at: Time.now.utc
+)
+
+consult = Consult.create(
+  canonical_name: "consult2",
+  name: "林毅夫",
+  thumbnail_large: "/consults/2.jpeg",
+  thumbnail_small: "/consults/2.jpeg",
+  published: true,
+  description: "bla bla bla...",
+  short_description: "bla bla bla...",
+  published_at: Time.now.utc
+)
+
+consult = Consult.create(
+  canonical_name: "consult3",
+  name: "王石",
+  thumbnail_large: "/consults/3.jpeg",
+  thumbnail_small: "/consults/3.jpeg",
+  published: true,
+  description: "bla bla bla...",
+  short_description: "bla bla bla...",
+  published_at: Time.now.utc
+)
+
+consult = Consult.create(
+  canonical_name: "consult4",
+  name: "刘强东",
+  thumbnail_large: "/consults/4.jpeg",
+  thumbnail_small: "/consults/4.jpeg",
+  published: true,
+  description: "bla bla bla...",
+  short_description: "bla bla bla...",
+  published_at: Time.now.utc
+)
+
+consult = Consult.create(
+  canonical_name: "consult5",
+  name: "黄奕",
+  thumbnail_large: "/consults/5.jpeg",
+  thumbnail_small: "/consults/5.jpeg",
+  published: true,
+  description: "bla bla bla...",
+  short_description: "bla bla bla...",
+  published_at: Time.now.utc
+)
+
+donator = Donator.create(
+  canonical_name: "donator1",
+  name: "中金公司",
+  thumbnail_large: "/donators/1.jpeg",
+  thumbnail_small: "/donators/1.jpeg",
+  published: true,
+  description: "bla bla bla...",
+  short_description: "bla bla bla...",
+  published_at: Time.now.utc
+)
+
+donator = Donator.create(
+  canonical_name: "donator2",
+  name: "摩根大通",
+  thumbnail_large: "/donators/2.jpeg",
+  thumbnail_small: "/donators/2.jpeg",
+  published: true,
+  description: "bla bla bla...",
+  short_description: "bla bla bla...",
+  published_at: Time.now.utc
+)
+
+donator = Donator.create(
+  canonical_name: "donator3",
+  name: "万科",
+  thumbnail_large: "/donators/3.jpeg",
+  thumbnail_small: "/donators/3.jpeg",
+  published: true,
+  description: "bla bla bla...",
+  short_description: "bla bla bla...",
+  published_at: Time.now.utc
+)
+
