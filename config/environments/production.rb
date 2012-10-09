f2d00495c10a9133a6857f4803a5de9f6939f7d3
caojia <jerrycircle@gmail.com>
2012-10-09 23:21:58 +0800

    send out emails on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,22 +61,22 @@ CunxinUi::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'cunxin.org' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false #TODO: should send out the email
+  config.action_mailer.perform_deliveries = true #TODO: should send out the email
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
+    address: "smtp.exmail.qq.com",
+    port: 25,
+    domain: "cunxin.org",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["MAIL_USER"],
+    password: ENV["MAIL_PASSWORD"]
   }
 
 
