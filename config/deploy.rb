26d1a8ed3cdb8a227f039b851ad4c313742eb7c0
caojia <jerrycircle@gmail.com>
2012-11-11 00:00:13 +0800

    cleanup releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,8 @@ set :branch, :develop
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
+after "deploy:restart", "deploy:cleanup"
+
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
