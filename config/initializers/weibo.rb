1cac4e7da02dbdc27adb192c831fbfb8595556d4
caojia <jerrycircle@gmail.com>
2012-11-11 00:08:56 +0800

    fix

--- config/initializers/weibo.rb
+++ config/initializers/weibo.rb
@@ -1,2 +1,2 @@
-WeiboOAuth2::Config.api_key = (WeiboOAuth2::Config.api_key || "3533041153")
+WeiboOAuth2::Config.api_key = (WeiboOAuth2::Config.api_key rescue "3533041153")
 WeiboOAuth2::Config.api_secret = "e08a64a86e44cc95ff4ef6b802930de1"
